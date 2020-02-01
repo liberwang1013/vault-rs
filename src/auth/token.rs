8dfc62448c9fff7c52014c788f945f4bd85637c7
Liber Wang <yili.wang@longbridge.sg>
2020-02-01 16:22:53 +0800

    add  method for token

--- src/auth/token.rs
+++ src/auth/token.rs
@@ -18,7 +18,23 @@ pub struct CreateTokenRequest {
     pub renewable: Option<bool>,
     #[serde(rename = "type")]
     pub token_type: String,
-    pub entity_alias: String
+    pub entity_alias: String,
+}
+
+#[derive(Serialize, Default)]
+pub struct LookupTokenRequest {
+    token: String,
+}
+
+#[derive(Serialize, Default)]
+struct RenewTokenRequest {
+    token: String,
+    increment: Option<String>,
+}
+
+#[derive(Serialize, Default)]
+struct RenewTokenSelfRequest {
+    increment: Option<String>,
 }
 
 impl VaultClient {
@@ -27,6 +43,76 @@ impl VaultClient {
         req: &CreateTokenRequest,
     ) -> crate::Result<VaultData<()>> {
         self.post("auth/token/create", req)
+            .await?
+            .parse::<VaultData<()>>()
+            .await
+    }
+
+    pub async fn create_token_orphan(
+        &self,
+        req: &CreateTokenRequest,
+    ) -> crate::Result<VaultData<()>> {
+        self.post("auth/token/create-orphan", req)
+            .await?
+            .parse::<VaultData<()>>()
+            .await
+    }
+
+    pub async fn create_token_with_role(
+        &self,
+        role: &str,
+        req: &CreateTokenRequest,
+    ) -> crate::Result<VaultData<()>> {
+        self.post(&format!("auth/token/create/{}", role), req)
+            .await?
+            .parse::<VaultData<()>>()
+            .await
+    }
+
+    pub async fn lookup_token(
+        &self,
+        req: LookupTokenRequest,
+    ) -> crate::Result<VaultData<()>> {
+        self.post("auth/token/lookup", req)
+            .await?
+            .parse::<VaultData<()>>()
+            .await
+    }
+
+    pub async fn lookup_token_self(&self) -> crate::Result<VaultData<()>> {
+        self.get("auth/token/lookup-self")
+            .await?
+            .parse::<VaultData<()>>()
+            .await
+    }
+
+    pub async fn renew_token(
+        &self,
+        token: &str,
+        increment: Option<&str>,
+    ) -> crate::error::Result<VaultData<()>> {
+        self.post(
+            "auth/token/renew",
+            &RenewTokenRequest {
+                token: String::from(token),
+                increment: increment.map(|i| String::from(i)),
+            },
+        )
+        .await?
+        .parse::<VaultData<()>>()
+        .await
+    }
+
+    pub async fn renew_token_self(
+        &self,
+        increment: Option<&str>,
+    ) -> crate::error::Result<VaultData<()>> {
+        self.post(
+            "auth/token/renew-self",
+            &RenewTokenSelfRequest {
+                increment: increment.map(|i| String::from(i)),
+            },
+        )
             .await?
             .parse::<VaultData<()>>()
             .await
